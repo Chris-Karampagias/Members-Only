8bc73872a0e2f5a3c66182876d1567f21b3d03d1
Chris <karampagiaschris@gmail.com>
2023-10-09 13:53:40 +0300

    Implement sign up functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 exports.memberAuth_get = asyncHandler((req, res, next) => {
   res.send("NOT IMPLEMENTED YET: member authentication get");
@@ -17,13 +19,88 @@ exports.login_post = asyncHandler((req, res, next) => {
   res.send("NOT IMPLEMENTED YET: login page post");
 });
 
-exports.signUp_get = asyncHandler((req, res, next) => {
-  res.send("NOT IMPLEMENTED YET: sign up page get");
-});
+exports.signUp_get = (req, res, next) => {
+  res.render("sign-up-form", {
+    isLoggedIn: false,
+    isMember: false,
+    user: undefined,
+    errors: null,
+  });
+};
 
-exports.signUp_post = asyncHandler((req, res, next) => {
-  res.send("NOT IMPLEMENTED YET: sign up page post");
-});
+exports.signUp_post = [
+  body("email")
+    .custom(async (value) => {
+      const user = await User.find({ email: value });
+      if (user.email === value) {
+        throw new Error();
+      } else {
+        return true;
+      }
+    })
+    .withMessage("Email is already in use")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Email is required")
+    .escape(),
+  body("firstName", "First name is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("lastName", "Last name is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password is required").trim().isLength({ min: 1 }).escape(),
+  body("passwordConfirm")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords do not match")
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    let isAdmin;
+    if (req.body.isAdmin) {
+      isAdmin = true;
+    } else {
+      isAdmin = false;
+    }
+    if (!errors.isEmpty()) {
+      const user = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        isMember: false,
+        isAdmin: isAdmin,
+      });
+      res.render("sign-up-form", {
+        isLoggedIn: false,
+        isMember: false,
+        user: user,
+        errors: errors.array(),
+      });
+    } else {
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if (err) {
+          return next(err);
+        }
+        const user = new User({
+          email: req.body.email,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          password: hashedPassword,
+          isMember: false,
+          isAdmin: isAdmin,
+        });
+        await user.save();
+      });
+
+      res.redirect("/clubhouse/login");
+    }
+  }),
+];
 
 exports.logout_get = asyncHandler((req, res, next) => {
   res.send("NOT IMPLEMENTED YET: logout page get");
